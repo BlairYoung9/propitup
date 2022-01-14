b809b91c3ba555602d9b223d1f297564b61d3f20
Blair Young <blairyoung009@gmail.com>
2022-01-14 15:29:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,23 @@ import PersonCard from "./components/PersonCard"
 
 
 function App() {
+
+  // const person = {
+  //   firstName: "Bob",
+  //   lastName : "Marley",
+  //   age: 33,
+  //   hairColor: "Black"
+  // }
   return (
     <div className="App">
       <h1>HELLO WORLD</h1>
-      <PersonCard/>
-      <PersonCard firstName  = "Bob"/>
-      <PersonCard lastName = "Marley"/>
-      <PersonCard age = "33"/>
-      <PersonCard hairColor = "Black"/>
+      <PersonCard firstName  = "Bob" lastName = "Marley" age = {33} hairColor = "Black"/>
       <hr></hr>
-      <PersonCard firstName  = "Bob"/>
-      <PersonCard lastName = "Marley2"/>
-      <PersonCard age = "33"/>
-      <PersonCard hairColor = "Black"/>
+      <PersonCard firstName  = "Bob" lastName = "Marley2" age = {33} hairColor = "Black"/>
       <hr></hr>
-      <PersonCard firstName  = "Bob"/>
-      <PersonCard lastName = "Marley3"/>
-      <PersonCard age = "33"/>
-      <PersonCard hairColor = "Black"/>
+      <PersonCard firstName  = "Bob" lastName = "Marley3" age = {33} hairColor = "Black"/>
       <hr></hr>
-      <PersonCard firstName  = "Bob"/>
-      <PersonCard lastName = "Marley4"/>
-      <PersonCard age = "33"/>
-      <PersonCard hairColor = "Black"/>
+      <PersonCard firstName  = "Bob" lastName = "Marley4" age = {33} hairColor = "Black"/>
     </div>
   );
 }
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,30 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        console.log(props);
+        // const{firstName,lastName,age,hairColor} = this.props.PersonCard;
+        this.state ={
+            age: this.props.age
+        }
+    }
+
+    increaseAge = () => {
+        console.log("clicked!")
+        this.setState({age:this.state.age +1})
+    }
+
     render() {
-        return <div>
-            {this.props.firstName}
-            {this.props.lastName}
-            {this.props.age}
-            {this.props.hairColor}
-            </div>;
+        return (
+            <div>
+                <p>{this.props.firstName}</p>
+                <p>{this.props.lastName}</p>
+                <p>{this.state.age}</p>
+                <p>{this.props.hairColor}</p>
+                <button onClick = {this.increaseAge}>Increase {this.props.firstName} age</button>
+            </div>
+        )
     }
 }
 
